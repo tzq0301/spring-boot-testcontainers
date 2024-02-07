22b12940c80c83606f055d5fc9ea94164560146c
Zhaoqi Tong <zhaoqi.tong@outlook.com>
2024-02-07 20:59:32 +0800

    optimize imports

--- src/main/java/org/example/springboottestcontainer/Controller.java
+++ src/main/java/org/example/springboottestcontainer/Controller.java
@@ -2,14 +2,7 @@ package org.example.springboottestcontainer;
 
 import jakarta.annotation.Resource;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.JdbcClient;
-import org.springframework.lang.NonNull;
-import org.springframework.transaction.TransactionManager;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.support.TransactionCallbackWithoutResult;
-import org.springframework.transaction.support.TransactionTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
